065bb2689a2f26acb2c3a930be751b8132507835
Long Tho <longtho638@gmail.com>
2026-07-04 21:54:23 +0700

    fix: remove all old Stitch UI components, keep only regenerated *New variants
    - Delete dead StitchLandingPage.tsx (unused page with old components)
    - Remove old component exports from index.ts (StitchHero, MenuGrid, Zones, Stats, etc.)
    - Keep only: layout components + regenerated *New components + StitchAbout
    - Clean barrel file: only export what's actually used

--- src/components/stitch/index.ts
+++ src/components/stitch/index.ts
@@ -1,3 +1,6 @@
+// Stitch Components — Only regenerated (New) + layout components
+// Old unversioned components removed — use *New variants
+
 export { default as StitchHeader } from './StitchHeader';
 export type { StitchHeaderProps } from './StitchHeader';
 
@@ -6,93 +9,8 @@ export type { StitchFooterProps } from './StitchFooter';
 
 export { default as StitchAppLayout } from './StitchAppLayout';
 
-export { default as StitchAdminDashboardV2 } from './StitchAdminDashboardV2';
-export type { StitchAdminDashboardV2Props, StatCardData, OrderData } from './StitchAdminDashboardV2';
-
-export { default as StitchMobileOrderingV2 } from './StitchMobileOrderingV2';
-export type { StitchMobileOrderingV2Props, MenuItem, CartItem } from './StitchMobileOrderingV2';
-
-export { default as StitchKDS } from './StitchKDS';
-export type { StitchKDSProps, Ticket, TicketItem, TicketStatus } from './StitchKDS';
-
-export { default as StitchAccountDashboard } from './StitchAccountDashboard';
-export type {
-  StitchAccountDashboardProps,
-  AccountProfile,
-  LoyaltyData,
-  TransactionData,
-  SubscriptionInfo,
-} from './StitchAccountDashboard';
-
-export { default as StitchCheckout } from './StitchCheckout';
-export type {
-  StitchCheckoutProps,
-  OrderItem,
-  OrderSummaryData,
-  OrderFormData,
-} from './StitchCheckout';
-
-export { default as StitchLoyalty } from './StitchLoyalty';
-export type {
-  StitchLoyaltyProps,
-  LoyaltyDashboardData,
-  RewardItem,
-  PointsHistoryEntry,
-  StreakDay,
-  TierBenefit,
-  LoadingState,
-} from './StitchLoyalty';
-
-export { default as StitchOrderSuccess } from './StitchOrderSuccess';
-export type {
-  StitchOrderSuccessProps,
-  OrderSuccessData,
-  OrderSuccessItem,
-  OrderStatus,
-} from './StitchOrderSuccess';
-
-export { default as StitchAdminLogin } from './StitchAdminLogin';
-export type { StitchAdminLoginProps, LoginStatus } from './StitchAdminLogin';
-
-export { default as StitchAdminOrders } from './StitchAdminOrders';
-export type { StitchAdminOrdersProps, Order, AdminOrderStatus, OrdersStats } from './StitchAdminOrders';
-
-export { default as StitchAdminPOS } from './StitchAdminPOS';
-export type { StitchAdminPOSProps, POSMenuItem, POSAddOn, POSCartItem } from './StitchAdminPOS';
-
 export { default as StitchAbout } from './StitchAbout';
-export type {
-  StitchAboutProps,
-  AboutPageData,
-  Zone,
-  TimelinePhase,
-  StoryCard,
-  ValueCard,
-} from './StitchAbout';
-
-export { default as StitchReviews } from './StitchReviews';
-export type {
-  StitchReviewsProps,
-  ReviewItem,
-  ReviewImage,
-  FilterKey,
-} from './StitchReviews';
-
-export { default as StitchEvents } from './StitchEvents';
-export type {
-  StitchEventsProps,
-  EventsPageData,
-  CalendarEvent,
-  PastEvent,
-  FeaturedEvent,
-} from './StitchEvents';
-
-export { default as StitchReferral } from './StitchReferral';
-export type {
-  StitchReferralProps,
-  ReferralFriend,
-  RewardRow,
-} from './StitchReferral';
+export type { StitchAboutProps, AboutPageData, Zone, TimelinePhase, StoryCard, ValueCard } from './StitchAbout';
 
 export { default as StitchLandingNew } from './StitchLandingNew';
 export type { StitchLandingNewProps } from './StitchLandingNew';
@@ -101,95 +19,37 @@ export { default as StitchStoryNew } from './StitchStoryNew';
 export type { StitchStoryNewProps, TeamMember } from './StitchStoryNew';
 
 export { StitchReviewsNew } from './StitchReviewsNew';
-export type {
-  StitchReviewsNewProps,
-  ReviewEntry,
-  ReviewImageData,
-  FilterOption,
-  ReviewsPageData,
-  LoadingStatus,
-} from './StitchReviewsNew';
+export type { StitchReviewsNewProps, ReviewEntry, ReviewImageData, FilterOption, ReviewsPageData, LoadingStatus } from './StitchReviewsNew';
 
 export { StitchReferralNew1 } from './StitchReferralNew1';
-export type {
-  StitchReferralNew1Props,
-  ReferralFriendEntry,
-  RewardHistoryRow,
-  ReferralLoadingState,
-} from './StitchReferralNew1';
+export type { StitchReferralNew1Props, ReferralFriendEntry, RewardHistoryRow, ReferralLoadingState } from './StitchReferralNew1';
 
 export { StitchEventsNew2 } from './StitchEventsNew2';
-export type {
-  StitchEventsNew2Props,
-  EventsNew2PageData,
-  EventCard2,
-  ArchiveEvent2,
-  FilterMonth,
-  NavLinkItem,
-} from './StitchEventsNew2';
+export type { StitchEventsNew2Props, EventsNew2PageData, EventCard2, ArchiveEvent2, FilterMonth, NavLinkItem } from './StitchEventsNew2';
 
 export { StitchContainerNew2 } from './StitchContainerNew2';
-export type {
-  StitchContainerNew2Props,
-  ContainerCafeNew2Data,
-  SignatureItem,
-  NavLink,
-  FeatureCard,
-  FooterLinkGroup,
-} from './StitchContainerNew2';
+export type { StitchContainerNew2Props, ContainerCafeNew2Data, SignatureItem, NavLink, FeatureCard, FooterLinkGroup } from './StitchContainerNew2';
 
 export { StitchAccountDashNew } from './StitchAccountDashNew';
-export type {
-  StitchAccountDashNewProps,
-  DashAccountProfile,
-  DashLoyaltyData,
-  DashOrderItem,
-} from './StitchAccountDashNew';
+export type { StitchAccountDashNewProps, DashAccountProfile, DashLoyaltyData, DashOrderItem } from './StitchAccountDashNew';
 
 export { StitchAccountNew } from './StitchAccountNew';
-export type {
-  StitchAccountNewProps,
-  AccountProfileNew,
-  LoyaltyDataNew,
-  OrderItemNew,
-  AccountCardNew,
-} from './StitchAccountNew';
+export type { StitchAccountNewProps, AccountProfileNew, LoyaltyDataNew, OrderItemNew, AccountCardNew } from './StitchAccountNew';
 
 export { StitchAdminLoginNew } from './StitchAdminLoginNew';
 export type { StitchAdminLoginNewProps } from './StitchAdminLoginNew';
 
 export { StitchAdminTerminalNew } from './StitchAdminTerminalNew';
-export type {
-  StitchAdminTerminalNewProps,
-  NavItemData,
-  NavSectionData,
-} from './StitchAdminTerminalNew';
+export type { StitchAdminTerminalNewProps, NavItemData, NavSectionData } from './StitchAdminTerminalNew';
 
 export { StitchCheckoutNew } from './StitchCheckoutNew';
-export type {
-  StitchCheckoutNewProps,
-  CheckoutNewItem,
-  CheckoutNewSummary,
-  CheckoutNewFormData,
-} from './StitchCheckoutNew';
+export type { StitchCheckoutNewProps, CheckoutNewItem, CheckoutNewSummary, CheckoutNewFormData } from './StitchCheckoutNew';
 
 export { StitchContainerNew1 } from './StitchContainerNew1';
-export type {
-  StitchContainerNew1Props,
-  ContainerCafeData,
-  NocturnalFeature,
-} from './StitchContainerNew1';
+export type { StitchContainerNew1Props, ContainerCafeData, NocturnalFeature } from './StitchContainerNew1';
 
 export { StitchEventsNew1 } from './StitchEventsNew1';
-export type {
-  StitchEventsNew1Props,
-  EventsPromoPageData,
-  PromotionCard,
-  ScheduleEvent,
-} from './StitchEventsNew1';
-
-export { default as StitchHero } from './StitchHero';
-export type { StitchHeroProps } from './StitchHero';
+export type { StitchEventsNew1Props, EventsPromoPageData, PromotionCard, ScheduleEvent } from './StitchEventsNew1';
 
 export { StitchHeroNew } from './StitchHeroNew';
 export type { StitchHeroNewProps } from './StitchHeroNew';
@@ -201,20 +61,10 @@ export { default as StitchLocation } from './StitchLocation';
 export type { StitchLocationProps } from './StitchLocation';
 
 export { StitchLoyaltyNew } from './StitchLoyaltyNew';
-export type {
-  StitchLoyaltyNewProps,
-  LoyaltyRewardItem,
-  LoyaltyHistoryEntry,
-  LoyaltyStreakDay,
-  LoyaltyTierBenefit,
-  LoyaltyLoadingState,
-} from './StitchLoyaltyNew';
+export type { StitchLoyaltyNewProps, LoyaltyRewardItem, LoyaltyHistoryEntry, LoyaltyStreakDay, LoyaltyTierBenefit, LoyaltyLoadingState } from './StitchLoyaltyNew';
 
 export { StitchMenu2New } from './StitchMenu2New';
 export type { StitchMenu2NewProps, MenuItem2Data } from './StitchMenu2New';
-
-export { default as StitchMenuGrid } from './StitchMenuGrid';
-export type { StitchMenuGridProps } from './StitchMenuGrid';
 
 export { StitchMenuNew } from './StitchMenuNew';
 export type { StitchMenuNewProps, MenuItemData } from './StitchMenuNew';
@@ -226,28 +76,10 @@ export { StitchOrderMgmtNew } from './StitchOrderMgmtNew';
 export type { StitchOrderMgmtNewProps } from './StitchOrderMgmtNew';
 
 export { StitchOrderSuccessNew } from './StitchOrderSuccessNew';
-export type {
-  StitchOrderSuccessNewProps,
-  OrderSuccessNewData,
-  OrderSuccessNewItem,
-} from './StitchOrderSuccessNew';
+export type { StitchOrderSuccessNewProps, OrderSuccessNewData, OrderSuccessNewItem } from './StitchOrderSuccessNew';
 
 export { StitchPOSNew } from './StitchPOSNew';
-export type {
-  StitchPOSNewProps,
-  POSNewMenuItem,
-  POSNewAddOn,
-  POSNewCartItem,
-} from './StitchPOSNew';
+export type { StitchPOSNewProps, POSNewMenuItem, POSNewAddOn, POSNewCartItem } from './StitchPOSNew';
 
 export { StitchReferralNew2 } from './StitchReferralNew2';
 export type { StitchReferralNew2Props } from './StitchReferralNew2';
-
-export { default as StitchStats } from './StitchStats';
-export type { StitchStatsProps } from './StitchStats';
-
-export { default as StitchTestimonials } from './StitchTestimonials';
-export type { StitchTestimonialsProps } from './StitchTestimonials';
-
-export { default as StitchZones } from './StitchZones';
-export type { StitchZonesProps } from './StitchZones';
